5c95b30c04a98d25f55cc7e00beff253661e4722
JuAnHePo <juanantonioherreraponferrada@gmail.com>
2025-03-10 16:13:32 +0100

    ejercicio2.scala modified

--- src/main/scala/ejercicio2.scala
+++ src/main/scala/ejercicio2.scala
@@ -1,23 +1,26 @@
-object ejercicio2 extends App:
+object ejercicio2 {
+  class cuentaBancaria {
+    private var saldo = 0
 
-  class CuentaBancaria(saldo: Double):
+    def verificarSaldo(): Int = saldo
 
-    def comprobarSaldo: Double =
-      saldo
+    def depositarFondos(fondos: Int): Unit = {
+      require(fondos > 0, "Cantidad a depositar inválida.\n")
+      saldo += fondos
+    }
 
-    def depositarDinero(saldoAIngresar: Double): CuentaBancaria =
-      CuentaBancaria(saldo + saldoAIngresar)
+    def retirarFondos(fondos: Int): Unit = {
+      require(fondos > 0 && fondos <= saldo, "Cantidad a retirar inválida.\n")
+      saldo -= fondos
+    }
+  }
 
-    def retirarDinero(saldoARetirar: Double): (Double, CuentaBancaria) =
-      if saldoARetirar < 0 || saldoARetirar > saldo then
-        throw Exception("La cantidad a retirar es invalida o el saldo de la cuenta insuficiente.")
-      else (saldoARetirar, CuentaBancaria(saldo - saldoARetirar))
-
-  var cuenta = CuentaBancaria(0)
-  printf("El saldo de la cuenta es: %2.2f.\n", cuenta.comprobarSaldo)
-  cuenta = cuenta.depositarDinero(10.5)
-  printf("El saldo de la cuenta es: %2.2f.\n", cuenta.comprobarSaldo)
-  val saldoRetirado = cuenta.retirarDinero(5.5)._1
-  cuenta = cuenta.retirarDinero(5.5)._2
-  printf("Despues de retirar %2.2f, el saldo de la cuenta es: %2.2f.\n", saldoRetirado, cuenta.comprobarSaldo)
-  cuenta.retirarDinero(6)
+  @main def mainPruebaCuentaBancaria(): Unit = {
+    val cuenta = cuentaBancaria()
+    printf("El saldo de la cuenta es: %d€.\n", cuenta.verificarSaldo())
+    cuenta.depositarFondos(100)
+    printf("El saldo de la cuenta es: %d€.\n", cuenta.verificarSaldo())
+    cuenta.retirarFondos(50)
+    printf("El saldo de la cuenta es: %d€.\n", cuenta.verificarSaldo())
+  }
+}
